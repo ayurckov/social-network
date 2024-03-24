6c10a4ea6297afe30af4a3029b2f1b87e4148ca6
ayurckov <yurckovama@gmail.com>
2024-03-24 10:51:14 +0500

    add-on-click

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
@@ -12,7 +12,6 @@ import Settings from './components/Settings/Settings';
 
 const App = (props) => {
   return (
-    <BrowserRouter>
     <div className='app-wrapper'>
       <Header />
       <Nav />
@@ -25,11 +24,9 @@ const App = (props) => {
           <Route path='/News' element={<News />} />
           <Route path='/Music' element={<Music />} />
           <Route path='/Settings' element={<Settings />} />
-
         </Routes>
       </div>
     </div>
-    </BrowserRouter>
     );
 }
 
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,13 +5,21 @@ import styles from "./MyPosts.module.css";
 const MyPosts = (props) => {
   let postElement = props.postsData.map((p) => <Post message={p.message} likes={p.likes}/>)
 
+
+  let newPostElement = React.createRef();
+
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text);
+  }
+
   return (
     <div className={styles.myPosts}>
       <h2>My posts</h2>
       <div>
-        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <textarea ref={newPostElement} cols="30" rows="10"></textarea>
         <div className="buttons">
-          <button>Add post</button>
+          <button onClick={ addPost }>Add post</button>
           <button>Remove post</button>
         </div>
       </div>
